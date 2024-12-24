70d22795706d0681b80d59767d53b5282326c690
Kaizen06 <umlyrithyreach@gmail.com>
2024-12-24 15:23:37 +0700

    add

---
+++ directTo.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('Listen_Now').click(function () {
+
+       $('link').attr('href', '../Main%20Page/index.html');
+    });
+    $('Songs').click(function () {
+        $('link').attr('href', '../Song(Playlist)/song.html');
+    })
+});
